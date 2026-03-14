1f3867d743ceee27f921587d105f7bac090e9129
dhruvish <mi.dhruvishpatel822@gmail.com>
2026-03-14 17:32:30 +0530

    faqs add

--- script.js
+++ script.js
@@ -36,3 +36,18 @@ prev.addEventListener("click", ()=>{
     updateSlider();
   }
 });
+
+
+const faqs = document.querySelectorAll(".faq-item");
+
+faqs.forEach(item => {
+
+const question = item.querySelector(".faq-question");
+
+question.addEventListener("click", () => {
+
+item.classList.toggle("active");
+
+});
+
+});
