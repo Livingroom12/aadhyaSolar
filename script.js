85b0ff7c49826db228347019cecb59e298d60d92
dhruvish <mi.dhruvishpatel822@gmail.com>
2026-03-17 00:01:44 +0530

    remove code

--- script.js
+++ script.js
@@ -58,16 +58,3 @@ item.classList.toggle("active");
 });
 
 });
-
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  const video = document.querySelector(".hero-video");
-
-  if (video) {
-    video.play().catch(function(error) {
-      console.log("Autoplay blocked:", error);
-    });
-  }
-});
